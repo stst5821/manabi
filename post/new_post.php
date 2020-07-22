8c39aa83a343e040331de9bd1442db2809088515
stst5821 <stst5821@gmail.com>
2020-07-22 11:08:46 +0900

    register_done完成

--- post/new_post.php
+++ post/new_post.php
@@ -4,7 +4,7 @@ require_once('../header.php');
 
 <h2>新規投稿</h2>
 <form method="post" action="new_post_done.php">
-    <textarea class="test" name="content" placeholder="本文を入力してください。" required="required"></textarea>
+    <textarea class="test" name="content" placeholder="まなびの記録を残しましょう！" required="required"></textarea>
     <br><br>
     <input type="submit" value="投稿する">
 
---
+++ register/register_done.php
@@ -0,0 +1,53 @@
+<?php
+require_once('../header.php');
+?>
+
+<?php
+
+try 
+{
+
+print_r($_POST);
+
+require_once('../common.php');
+$post = sanitize($_POST);
+
+$name = $post['name'];
+$email = $post['email'];
+$password = $post['password'];
+
+$dsn = 'mysql:dbname=manabi;host=localhost;charset=utf8';
+$user = 'root';
+$password = '1234';
+$dbh = new PDO ($dsn,$user,$password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$sql = 'INSERT INTO member(name,email,password) VALUES(?,?,?)';
+$stmt = $dbh->prepare($sql);
+$data[] = $name;
+$data[] = $email;
+$data[] = $password;
+$stmt->execute($data);
+
+$dbn = null;
+
+print '会員登録できました！';
+
+}
+catch(Exception $e) 
+{
+    print 'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+}
+
+
+?>
+
+
+
+<br><br>
+<a href="post_list.php">トップへ戻る</a>
+
+<?php
+require_once('../footer.php');
+?>
